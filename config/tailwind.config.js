89c417eacb7ce49db9b839a031179f6b6333ad38
sariabueitah <sari.abueitah@gmail.com>
2024-12-17 03:08:47 +0300

    update to rails 8

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,17 +5,13 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-    './config/initializers/*.rb'
+    './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-      colors: {
-
-      }
     },
   },
   plugins: [
